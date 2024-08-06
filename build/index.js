11885c96c6d337f547a0497c7b3975c829cf6809
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-08-05 20:03:06 -0400

    up

--- build/index.js
+++ build/index.js
@@ -1,27 +1,27 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const config_1 = require("./database/config");
-const index_1 = require("./routes/index");
-const PORT = process.env.PORT;
+'use strict'
+const __importDefault = (this && this.__importDefault) || function (mod) {
+  return (mod && mod.__esModule) ? mod : { default: mod }
+}
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+const cors_1 = __importDefault(require('cors'))
+const config_1 = require('./database/config')
+const index_1 = require('./routes/index')
+const PORT = process.env.PORT
 const app = (0, express_1.default)();
 // db
 (0, config_1.dbConnection)()
-    .then(() => {
+  .then(() => {
     // middlewares
-    app.use(express_1.default.json());
-    app.use((0, cors_1.default)({ origin: 'https://raizo-ecommerce.netlify.app' }));
+    app.use(express_1.default.json())
+    app.use((0, cors_1.default)({ origin: '*' }))
     // routes
-    app.use('/products', index_1.routerProduct);
-    app.use('/categories', index_1.routerCategory);
+    app.use('/products', index_1.routerProduct)
+    app.use('/categories', index_1.routerCategory)
     app.listen(PORT, () => {
-        console.log(`Listening in PORT http://localhost:${PORT}`);
-    });
-})
-    .catch((error) => {
-    console.log('Error', error.message);
-});
+      console.log(`Listening in PORT http://localhost:${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log('Error', error.message)
+  })
